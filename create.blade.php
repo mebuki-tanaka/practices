650358b812da7f68bb5ec611c2411cba2a226ad4
mebuki-tanaka <61735267+mebuki-tanaka@users.noreply.github.com>
2020-06-23 19:16:58 +0900

    PHP/Laravel 12 – [Mac編]Bladeテンプレートの継承と〜

--- create.blade.php
+++ create.blade.php
@@ -1,13 +1,36 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-    <title>MyProfile</title>
-  </head>
-  <body>
-    <h1>自己紹介</h1>
-  </body>
-</html>
+{{-- profile,blade.phpの@yield('title')に’Myprofile’を埋め込む --}}
+@section('title', 'Myprofile')
+
+{{--  profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>My プロフィール</h2>
+        <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+          @if (count($errors) > 0 )
+            <ul>
+              @foreach($errors->all() as $e)
+                <li>{{ $e }}</li>
+              @endforeach
+            </ul>
+          @endif
+          <div class="form-group row">
+            <label class="col-md-2" for="name">氏名</label>
+            <div class="col-md-6">
+              <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+            </div>
+            <label class="col-md-2" for="gender">性別</label>
+            <div class="col-md-2">
+              <input type="radio" name="性別"  value="男"/>男
+              <input type="radio" name="性別" value="女"/>女
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+@endsection

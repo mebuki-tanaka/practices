5556ef858db2ded2d10fb300ca4e91f614dff7e9
mebuki-tanaka <61735267+mebuki-tanaka@users.noreply.github.com>
2020-06-23 19:24:20 +0900

    PHP/Laravel 13 – [Mac編]ユーザー認証を実装する

--- web.php
+++ web.php
@@ -17,12 +17,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 Route::get('XXX' , 'Admin\AAAController@bbb');
 
 Route::group(['prefix' => 'admin/profile'], function(){
-    Route::get('create', 'Admin\ProfileController@add');
-    Route::get('edit' , 'Admin\ProfileController@edit');
+    Route::get('create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::post('create' , 'Admin\ProfileController@create');
+    Route::get('edit' , 'Admin\ProfileController@edit')->middleware('auth');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
